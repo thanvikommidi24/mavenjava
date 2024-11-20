ad5329aefbbedc866b2816010c9ef78aee3a374e
thanvi <thanvikommidi24@gmail.com>
2024-11-20 09:50:25 +0530

    Add new files copied via Finder

---
+++ src/main/java/SEGroup/MvenJavaProject/App.java
@@ -0,0 +1,7 @@
+package SEGroup.MvenJavaProject;
+public class App {
+	public static void main (String[] args)
+	{
+		System.out.println("this is first maven java project thanvi 22bd1a665r");
+	}
+}
